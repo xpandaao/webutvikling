0dec7f8f452f689df0d0e00d737c8d97727c6035
xpandaao <ida.marie132@gmail.com>
2022-09-26 09:39:29 +0200

    Added files for assignment

---
+++ css-grid/javascript/ham.js
@@ -0,0 +1,11 @@
+const menu = document.getElementById("main-menu")
+
+const hamMenu = () =>{
+    if(menu.style.display === "block"){
+        menu.style.display = "none"
+    }else{
+        menu.style.display = "block"
+    }
+}
+
+
